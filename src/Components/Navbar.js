8ed8ca395904473fef74c963a874f4452eca6aee
Javeriya Imran <121876323+jennylewis45@users.noreply.github.com>
2023-10-23 19:44:24 +0500

    Add files via upload

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+  <>
+  <a href="/">Home</a>
+  <br />
+  <a href="/about">About</a>
+  <br />
+<a href="/contact">Contact</a>
+<br />
+<a href="/shop">Shop</a>
+
+  </>
+  )
+}
+
+export default Navbar
---
+++ src/Components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Shop() {
+  return (
+   <>
+   <h1>I am Shop Page</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, quia!</p>
+   <h1>jjxjs</h1>
+   <p>hsajxkclkajckxas</p>
+   </>
+  )
+}
+
+export default Shop
