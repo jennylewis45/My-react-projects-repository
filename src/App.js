e9f045edc41e8ebde1860490567fe805701806de
Javeriya Imran <121876323+jennylewis45@users.noreply.github.com>
2023-10-23 19:47:35 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react'
-import Calculator from './Components/Calculator'
+// import Calculator from './Components/Calculator'
+// import Calculators from'./Components/Calculators'
+// import Counter from './Components/Counter';
+// import Passgenerator from './Components/Passgenerator';
+
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Shop from './Components/Shop';
+import{BrowserRouter as Main,Routes,Route}from "react-router-dom"
+
+
+
 
 function App() {
   return (
+
     <>
-    <Calculator title="Change your background color with your own choice"/>
-        <Calculators title="My Calculator"/>
-        <Passgenerator/>
+
+   
+    {/* <Calculator title="Change your background color with your own choice"/> */}
+    {/* <Calculators title="My Calculator"/> */}
+    {/* <Counter/> */}
+
+    {/* <Passgenerator/> */}
+    <Main>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+<Route path='/about'element={<About/>}/>
+<Route path='/contact' element={<Contact/>}/>   
+<Route path='/shop' element={<Shop/>}/>
+    </Routes>
+
+
+    </Main>
+ 
 
 
     </>
